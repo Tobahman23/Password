cc5df5f0402af32244a468ce575302ad0b5153f7
Tobahman23 <84333560+Tobahman23@users.noreply.github.com>
2021-10-01 09:11:20 +0200

    yoyo
    oyoy

--- index.js
+++ index.js
@@ -17,15 +17,68 @@ var repe = document.getElementById("rep");
 var sub = document.getElementById("sube");
 let bol = new Boolean(false);
 var che = document.getElementById("chec");
+var com = 0;
+var chec = 0;
+var ech = 0;
 if (sub.clicked == true)
 {
-
+checkwords(pas);
+checkwords(repe);
+comparing(pas, repe);
+emailcheck(ema);
+if (bol == true && che.checked == true)
+{
+window.location.href="indexus.html";
+}
+else {
+if (com = 1)
+{
+    document.getElementById("a").innerHTML = "You need to have the same password twice!";
+}
+else if (chec = 1)
+{
+    document.getElementById("a").innerHTML = "You need to agree to the terms!";
+}
+else if (ech = 1)
+{
+document.getElementById("a").innerHTML = "You nede to write a correct email adress!";
+}
+}
 }
 
 function comparing(c, d)
 {
-if (c != d)
+if (c == d)
 {
 bol(true);
+}
+else 
+{
+bol(false);
+com = 1;
+}
+}
+
+function checkwords(e)
+{
+if (e.length >= 6 )
+{
+bol(true);
+}
+else {
+    bol(false);
+    chec = 1;
+}
+}
+
+function emailcheck(f)
+{
+if (f.indexOf('@') == -1)
+{
+bol(true);
+}
+else {
+    bol(false);
+    ech = 1;
 }
 }
