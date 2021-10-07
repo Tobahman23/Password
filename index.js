a9e7a5db6baf1a43b9ef939c9f1824c656834d8d
Tobahman23 <84333560+Tobahman23@users.noreply.github.com>
2021-10-07 14:25:08 +0200

    wo
    ow

--- index.js
+++ index.js
@@ -10,30 +10,27 @@ console.log(arr); //Skriver ut arrayen i konsollen
 document.getElementById("b").innerHTML = arr; //Gör om den andra h3 till arrayen
 //klar med första uppgiften här
 
-var use = document.getElementById("user"); //skapar variabler för alla inputs
-var ema = document.getElementById("emaill");
-var pas = document.getElementById("pass");
-var repe = document.getElementById("rep");
-var sub = document.getElementById("sube");
+ //skapar variabler för alla inputs
+const ema = document.getElementById("exampleInputEmail1");
+const sub = document.getElementById("sube");
 let bol = new Boolean(false); //skapar bool
-var che = document.getElementById("chec");
+const che = document.getElementById("exampleCheck1");
 var com = 0; //skapar variabler för att kolla om något blir fel i inmatningen
-var chec = 0;
-var ech = 0;
+var chec = 0;  //skapar variabler för att kolla om något blir fel i inmatningen 
+var ech = 0; //skapar variabler för att kolla om något blir fel i inmatningen
 function validera() //huvudfunktionen 
 {
-checkwords(pas); //anropar funktioner som ska konfirmera ifall allt är korrekt
-checkwords(repe);
-comparing(pas, repe);
-emailcheck(ema);
-if (bol == true && che.checked == true) //körs ifall inget är fel
+    var pas = document.getElementById("exampleInputPassword1");
+    var repe = document.getElementById("exampleInputPassword2");
+    const userd = document.getElementById("formGroupExampleInput");
+if (bol == false && che.checked == true) //körs ifall inget är fel
 {
 location.href= 'indexus.html'; 
 }
 else { //annars körs denna som kollar vad exakt som är fel och säger till användaren vad som är fel
 if (com = 1)
 {
-    document.getElementById("a").innerHTML = "You need to have the same password twice!";
+    document.getElementById("a").innerHTML = "You need to have the same password twice and include 6 or more characters!";
 }
 else if (chec = 1)
 {
@@ -41,44 +38,21 @@ else if (chec = 1)
 }
 else if (ech = 1)
 {
-document.getElementById("a").innerHTML = "You nede to write a correct email adress!";
+document.getElementById("a").innerHTML = "You need to write a valid email adress!";
 }
 }
 }
 
 function comparing(c, d) //funktion för att jämföra lösenord så det repeterade lösenordet är lika
 {
-if (c == d)
+if (c == d && c.length>=6)
 {
-bol(true);
+bol = true;
 }
 else 
 {
-bol(false);
+bol = false;
 com = 1;
 }
 }
-
-function checkwords(e) //funktion för att kolla så att lösenordet är mer än 6 karaktärer
-{
-if (e.length > 6 )
-{
-bol(true);
-}
-else {
-    bol(false);
-    chec = 1;
-}
-}
-
-function emailcheck(f) //kollar ifall ett snabel a finns med i mailen för att validera att det är en mail
-{
-if (f.indexOf('@') == -1)
-{
-bol(true);
-}
-else {
-    bol(false);
-    ech = 1;
-}
-}
+//tog bort emailchecken i js då den inte funka så bra och jag har en fungerande i html
